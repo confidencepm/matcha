cdbf782ac105c6a2d5ad3a4225cac5c951d2ab9b
martian1431 <pmalope@student.wethinkcode.co.za>
2020-06-25 19:56:37 +0200

    use localhost instead of ip address

--- flask/matcha/views/profile.py
+++ flask/matcha/views/profile.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, session, redirect, flash, request, url_for
 from functools import wraps
 import os, secrets, re, html, pymongo, bcrypt
+
 # from werkzeug import secure_filename
 from PIL import Image
 from matcha import db, app, logged_in_users
@@ -10,133 +11,146 @@ from bson.objectid import ObjectId
 from datetime import date
 from bson import ObjectId
 
-user = Blueprint('profile', __name__)
+user = Blueprint("profile", __name__)
 
-@user.route('/profile',  methods=['GET', 'POST'])
+
+@user.route("/profile", methods=["GET", "POST"])
 @login_required
 def profile():
-    user = db.get_user({'username' : session.get('username')})
+    user = db.get_user({"username": session.get("username")})
     admin = False
 
-    if user['_id'] == ObjectId(b'bobisadmin!!'):
+    if user["_id"] == ObjectId(b"bobisadmin!!"):
         admin = True
     errors = []
     location = []
-    blocked = user['blocked']
-    users=db.users({'_id' : { '$nin' : blocked }, 'completed' : 1})
-    if user['completed'] == 1:
-       valid_users = [check_user for check_user in users if check_user['completed'] == 1 and get_howfar(user, check_user) < 20]
+    blocked = user["blocked"]
+    users = db.users({"_id": {"$nin": blocked}, "completed": 1})
+    if user["completed"] == 1:
+        valid_users = [
+            check_user
+            for check_user in users
+            if check_user["completed"] == 1 and get_howfar(user, check_user) < 20
+        ]
     else:
         valid_users = []
 
-    #update user details
-    if request.method == 'POST':
-        if request.form.get('submit') == 'update':
+    # update user details
+    if request.method == "POST":
+        if request.form.get("submit") == "update":
             print(f"Debug {request.form}")
-            username = html.escape(request.form.get('username'))
-            email = html.escape(request.form.get('email'))
-            firstname = html.escape(request.form.get('firstname'))
-            lastname = html.escape(request.form.get('lastname'))
-            image_file = request.files.get('image')
-                           
-            if not re.match('^[A-Za-z][A-Za-z0-9]{2,49}$', username):
-                errors.append('The username must be an alpha numeric value, 3 - 50 characters long.')
-            elif user['username'] != username and db.get_user({'username': username}):
+            username = html.escape(request.form.get("username"))
+            email = html.escape(request.form.get("email"))
+            firstname = html.escape(request.form.get("firstname"))
+            lastname = html.escape(request.form.get("lastname"))
+            image_file = request.files.get("image")
+
+            if not re.match("^[A-Za-z][A-Za-z0-9]{2,49}$", username):
+                errors.append(
+                    "The username must be an alpha numeric value, 3 - 50 characters long."
+                )
+            elif user["username"] != username and db.get_user({"username": username}):
                 errors.append("The username is already taken please choose another")
             else:
-                user['username'] = username
-                session['username'] = username
+                user["username"] = username
+                session["username"] = username
 
-            if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,100}$', email):
-                errors.append('invalid email format')
-            elif user['email'] != email and db.get_user({'email' : email}):
+            if not re.match(
+                r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,100}$", email
+            ):
+                errors.append("invalid email format")
+            elif user["email"] != email and db.get_user({"email": email}):
                 errors.append("The email is already taken please choose another one")
             else:
-                user['email'] = email
+                user["email"] = email
             if not firstname:
-                errors.append('Firstname may not be empty')
-            elif not re.match('^[A-Z][A-Za-z-]{2,24}$', firstname):
-                errors.append('Firstname must begin with a capital letter and contain only: letters and/or -\'s')
+                errors.append("Firstname may not be empty")
+            elif not re.match("^[A-Z][A-Za-z-]{2,24}$", firstname):
+                errors.append(
+                    "Firstname must begin with a capital letter and contain only: letters and/or -'s"
+                )
             else:
-                user['firstname'] = firstname
+                user["firstname"] = firstname
 
             if not lastname:
-                errors.append('Lastname may not be empty')
-            elif not re.match('^[A-Z][ A-Za-z-]{2,24}$', lastname):
-                errors.append('Lastname must begin with a capital letter and contain only: letters, spaces and/or -\'s')
+                errors.append("Lastname may not be empty")
+            elif not re.match("^[A-Z][ A-Za-z-]{2,24}$", lastname):
+                errors.append(
+                    "Lastname must begin with a capital letter and contain only: letters, spaces and/or -'s"
+                )
             else:
-                user['lastname'] = lastname
+                user["lastname"] = lastname
 
             if image_file:
                 pic_name = save_picture(image_file)
-                user['image_name'] = pic_name
+                user["image_name"] = pic_name
 
             if not errors:
-                db.update_user(user['_id'], user)
-                flash("User details updated", 'success')
-                return redirect( url_for('profile.profile') )
+                db.update_user(user["_id"], user)
+                flash("User details updated", "success")
+                return redirect(url_for("profile.profile"))
 
             for error in errors:
-                flash(error, 'danger')
+                flash(error, "danger")
 
-        #update user password
-        if request.form.get('submitPwd') == 'update':
+        # update user password
+        if request.form.get("submitPwd") == "update":
             # print(f"Debug {request.form}")
-            password = html.escape(request.form.get('current_password'))
-            new_password = html.escape(request.form.get('new_password'))
-            check_new_password = html.escape(request.form.get('new_password_repeat'))
+            password = html.escape(request.form.get("current_password"))
+            new_password = html.escape(request.form.get("new_password"))
+            check_new_password = html.escape(request.form.get("new_password_repeat"))
             # print("Debug2 ", password, new_password, check_new_password)
-            if not bcrypt.checkpw(password.encode('utf-8'), user['password']):
-                errors.append('Incorrect password')
+            if not bcrypt.checkpw(password.encode("utf-8"), user["password"]):
+                errors.append("Incorrect password")
             elif new_password:
-                #fixme
+                # fixme
                 # if not re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{5,25}$", new_password):
                 #     print("Password error")
                 #     errors.append('The password must have an uppercase, lowercase and a digit')
                 if check_new_password != new_password:
-                    errors.append('The two passwords do not match')
+                    errors.append("The two passwords do not match")
                 else:
                     salt = bcrypt.gensalt()
-                    user['password'] = bcrypt.hashpw(new_password.encode('utf-8'), salt)
-                    db.update_user(user['_id'], user)
-            
+                    user["password"] = bcrypt.hashpw(new_password.encode("utf-8"), salt)
+                    db.update_user(user["_id"], user)
+
             if not errors:
                 print("Updating password")
-                db.update_user(user['_id'], user)
-                flash("Password updated", 'success')
+                db.update_user(user["_id"], user)
+                flash("Password updated", "success")
             else:
                 for error in errors:
-                    flash(error, 'danger')
+                    flash(error, "danger")
 
-        #update profile
-        if request.form.get('submit') == 'bioupdate':
-            gender = request.form.get('gender')
-            sex = request.form.get('sexo')
-            intret = request.form.getlist('interests')
-            bio = html.escape(request.form.get('bio'))
-            image_file = request.files.get('image2')
+        # update profile
+        if request.form.get("submit") == "bioupdate":
+            gender = request.form.get("gender")
+            sex = request.form.get("sexo")
+            intret = request.form.getlist("interests")
+            bio = html.escape(request.form.get("bio"))
+            image_file = request.files.get("image2")
             if not intret:
                 intret = ["none"]
             if not bio:
-                errors.append('Bio may not be empty')
+                errors.append("Bio may not be empty")
             elif len(bio) > 500:
-                errors.append('The Bio may not be longer than 500 characters')
+                errors.append("The Bio may not be longer than 500 characters")
             else:
-                user['bio'] = bio
+                user["bio"] = bio
 
-            if image_file :
+            if image_file:
                 pic_name = save_picture(image_file)
-                user['image_name'] = pic_name
+                user["image_name"] = pic_name
 
-            elif user['image_name'] == "default.png":
-                errors.append('Please update your photo.')
+            elif user["image_name"] == "default.png":
+                errors.append("Please update your photo.")
 
             if not errors:
-                user['gender'] = gender
-                user['sex'] = sex
-                user['interests'] = intret
-                user['completed'] = 1
-                location = request.form.get('location')
+                user["gender"] = gender
+                user["sex"] = sex
+                user["interests"] = intret
+                user["completed"] = 1
+                location = request.form.get("location")
                 print(f"Location: {location}")
                 # latlon1 = request.form.get('latlon')
                 location = location.split(",")
@@ -150,54 +164,70 @@ def profile():
                 # else:
                 lat = location.pop(3)
                 lon = location.pop(3)
-                user['location'] = location
-                user['latlon'] = [lat, lon]
-                db.update_user(user['_id'], user)
-                flash('Profile updated', 'success')
-                return redirect( url_for('profile.profile'))
+                user["location"] = location
+                user["latlon"] = [lat, lon]
+                db.update_user(user["_id"], user)
+                flash("Profile updated", "success")
+                return redirect(url_for("profile.profile"))
 
             for error in errors:
-                flash(error, 'danger')
+                flash(error, "danger")
 
-        if request.form.get('submit') == 'Upload':
+        if request.form.get("submit") == "Upload":
             # print(valid_users)
-            total_amnt = len(user['gallery'])
+            total_amnt = len(user["gallery"])
             if total_amnt < 4:
-                image_file = request.files.get('image3')
+                image_file = request.files.get("image3")
                 if image_file:
                     gallery_img = save_gallery(image_file)
-                    user['gallery'].append(gallery_img)
+                    user["gallery"].append(gallery_img)
 
-                    db.update_user(user['_id'], user)
-                    return redirect( url_for('profile.profile') )
+                    db.update_user(user["_id"], user)
+                    return redirect(url_for("profile.profile"))
             else:
-                flash('You can only have 4 pictures in your gallery', 'danger')
+                flash("You can only have 4 pictures in your gallery", "danger")
 
     viewers = []
-    for id in user['views']:
-        viewers.append(db.get_user({'_id': ObjectId(id)}))
+    for id in user["views"]:
+        viewers.append(db.get_user({"_id": ObjectId(id)}))
 
-    flirts = []
-    for username in user['flirted']:
-        flirts.append(db.get_user({'username': username}))
+    likes = []
+    for username in user["flirted"]:
+        likes.append(db.get_user({"username": username}))
 
     matched = []
-    for username in user['matched']:
-        matched.append(db.get_user({'username': username}))
+    for username in user["matched"]:
+        matched.append(db.get_user({"username": username}))
 
     online_users = list(logged_in_users.keys())
-    return render_template('user/profile.html', logged_in=session.get('username'), current_user=user, users=valid_users, admin=admin, viewers=viewers, flirts=flirts, matched=matched, online_users=online_users )
+    return render_template(
+        "user/profile.html",
+        logged_in=session.get("username"),
+        current_user=user,
+        users=valid_users,
+        admin=admin,
+        viewers=viewers,
+        likes=likes,
+        matched=matched,
+        online_users=online_users,
+    )
 
 
-
-@user.route('/profile/view/<user_id>')
+@user.route("/profile/view/<user_id>")
 @login_required
 @finish_profile
 def view_profile(user_id):
     id = ObjectId(user_id)
-    current_user = db.get_user({'username': session.get('username')})
+    current_user = db.get_user({"username": session.get("username")})
 
-    user = db.get_user({'_id': id})
-    user['fame-rating'] = int((user['fame-rating'] / 10) - 1)
+    user = db.get_user({"_id": id})
+    user["fame-rating"] = int((user["fame-rating"] / 10) - 1)
     online_users = list(logged_in_users.keys())
-    return render_template('user/view_profile.html', logged_in=session.get('username'), user=user, current_user=current_user, online_users=online_users)
+    return render_template(
+        "user/view_profile.html",
+        logged_in=session.get("username"),
+        user=user,
+        current_user=current_user,
+        online_users=online_users,
+    )
+
