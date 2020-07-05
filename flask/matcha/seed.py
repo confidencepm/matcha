3408798ca24b795d8982938188fa4c91fc758864
martian1431 <pmalope@student.wethinkcode.co.za>
2020-07-05 14:58:53 +0200

    bug fix

--- flask/matcha/seed.py
+++ flask/matcha/seed.py
@@ -41,7 +41,7 @@ def create_fakes():
 
         max_interests = fake.random_int(3, 9)
         user_info['interests'] = random.sample(interests, max_interests)
-        user_info['fame-rating'] = fake.random_int(0, 80)
+        user_info['fame-rating'] = fake.random_int(5, 90)
         user_info['location'].append(''.join([str(fake.random_int(1, 500)), ' ', fake.word(), ' street']))
         user_info['location'].append(''.join([fake.word(), 'cliff']))
         user_info['location'].append(random.choice(city))
